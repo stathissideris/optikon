b0348927b7429a6c1a1cda1043471b94c11d48c1
Stathis Sideris <stathis@pixelated-noise.com>
2019-09-14 15:51:40 +0300

    Add support for EDN input

--- src/optikon/vega.clj
+++ src/optikon/vega.clj
@@ -1,5 +1,8 @@
 (ns optikon.vega
-  (:require [optikon.js :as js]))
+  (:require [optikon.js :as js]
+            [cheshire.core :as json]
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
 
 (set! *warn-on-reflection* true)
 
@@ -11,8 +14,12 @@
     ctx))
 
 (defn render-svg [in-filename out-filename]
-  (let [ctx (context)]
-    (->> (str "x = render(" (slurp in-filename) ")")
+  (let [ctx   (context)
+        input (slurp in-filename)
+        input (if (str/ends-with? in-filename ".edn")
+                (-> input edn/read-string json/generate-string)
+                input)]
+    (->> (str "render(" input ")")
          (js/eval ctx)
          .asHostObject
          deref
