9e5450e9952a2dd0ddbb25316784cbed1fee778e
Stathis Sideris <stathis@pixelated-noise.com>
2019-09-14 13:01:36 +0300

    Remove dummy atom

--- src/optikon/cli.clj
+++ src/optikon/cli.clj
@@ -2,8 +2,5 @@
   (:require [optikon.vega :as vega])
   (:gen-class))
 
-(def dummy (atom nil))
-
 (defn -main [in-filename out-filename]
-  (reset! dummy 1)
   (vega/render-svg in-filename out-filename))
